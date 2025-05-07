4400bfce527f23929a198698af485cdcb677a2d4
Aizekhan <hrytsenkomaksym@gmail.com>
2025-05-07 15:33:03 +0300

    1

--- Assets/_MythHunter/Resources/Pool/PoolStatistics.cs
+++ Assets/_MythHunter/Resources/Pool/PoolStatistics.cs
@@ -1,3 +1,4 @@
+// Шлях: Assets/_MythHunter/Code/Resources/Pool/PoolStatistics.cs
 using System;
 
 namespace MythHunter.Resources.Pool
@@ -7,13 +8,28 @@ namespace MythHunter.Resources.Pool
     /// </summary>
     public class PoolStatistics
     {
-        public string PoolType { get; set; } = "";
+        // Базова інформація
+        public string PoolType { get; set; } = string.Empty;
         public DateTime CreationTime { get; set; } = DateTime.Now;
         public int InitialSize { get; set; } = 0;
+
+        // Поточний стан
         public int ActiveCount { get; set; } = 0;
         public int InactiveCount { get; set; } = 0;
-        public int TotalSize => ActiveCount + InactiveCount;
+        public int TotalSize { get; set; } = 0;
+
+        // Метрики використання
         public long TotalGetCount { get; set; } = 0;
         public long TotalReturnCount { get; set; } = 0;
+
+        // Додаткова інформація для діагностики
+        public float GetPerSecond { get; set; } = 0;
+        public float ReturnPerSecond { get; set; } = 0;
+
+        public override string ToString()
+        {
+            return $"Pool: {PoolType}, Active: {ActiveCount}, Inactive: {InactiveCount}, " +
+                   $"Total: {TotalSize}, Gets: {TotalGetCount}, Returns: {TotalReturnCount}";
+        }
     }
 }
