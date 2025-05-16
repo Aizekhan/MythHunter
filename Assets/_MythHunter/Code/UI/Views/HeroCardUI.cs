a37033d18328df4e8a46dd56280a72d387256c4d
Aizekhan <hrytsenkomaksym@gmail.com>
2025-05-16 03:16:58 +0300

    defIconHero

--- Assets/_MythHunter/Code/UI/Views/HeroCardUI.cs
+++ Assets/_MythHunter/Code/UI/Views/HeroCardUI.cs
@@ -33,7 +33,7 @@ namespace MythHunter.UI.Views
             _descriptionText.text = model.Description;
             _raceClassText.text = $"{model.Race} - {model.Class}";
             _manaCostText.text = $"Вартість: {model.ManaCost}";
-
+            _defaultIcon = UnityEngine.Resources.Load<Sprite>("UI/Icons/default_hero");
             Sprite icon = null;
             if (!string.IsNullOrEmpty(model.IconPath))
             {
